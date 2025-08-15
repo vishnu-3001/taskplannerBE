cffd04bf1764cf1e628ab17127394e186d9e10c0
vishnu-3001 <vishnusai3001@yahoo.com>
2025-08-15 16:38:55 -0400

    cre3atd tasklets api

--- taskplanner/src/main/java/com/taskplanner/taskplanner/controller/TaskController.java
+++ taskplanner/src/main/java/com/taskplanner/taskplanner/controller/TaskController.java
@@ -3,14 +3,16 @@ package com.taskplanner.taskplanner.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.taskplanner.taskplanner.service.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import com.taskplanner.taskplanner.dto.*;
 import java.util.*;
 
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000") // Allow requests from this origin
 public class TaskController {
     private TaskService tService;
     public TaskController(TaskService service){
